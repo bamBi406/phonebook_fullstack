4a5a27264e70e92dbfd3cc109851f9a0bc2dc6e8
bamBi406 <antoine.esponda@hotmail.fr>
2025-08-29 15:35:49 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 const unknownEndpoint = (request, response) => {
@@ -22,7 +21,6 @@ morgan.token('reqBody', (req) => {
     return JSON.stringify(bodyContent)
 })
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
